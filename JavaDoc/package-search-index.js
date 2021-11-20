ed371220de1d05f8b4b47b13061c81121fb47883
Jade <boonejade777@gmail.com>
2021-11-19 19:24:56 -0500

    New Addition

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.javafxpractice","l":"com.example.javafxpractice"}];updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.javafxpractice","l":"HelloApplication"},{"p":"com.example.javafxpractice","l":"HelloController"}];updateSearchResults();
